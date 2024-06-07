f38f9df12d22ad3b57553e0388543a28c3eb18bb
SrSleep <alejovastillo@gmail.com>
2024-06-07 00:53:46 -0500

    se uso la api de minhub para traer los datos en las demas paginas

--- upComingEvents.js
+++ upComingEvents.js
@@ -199,30 +199,37 @@ const data = {
 let cartasDiv = document.getElementById(`contenedorCartas`);
 let checkboxDiv = document.getElementById(`contendorCheckbox`);
 let inputBuscar = document.getElementById(`buscar`);
+let urlApiEvents = "https://aulamindhub.github.io/amazing-api/events.json"
+fetch(urlApiEvents)
+    .then(response => response.json())
+    .then(data => {
 
-functionsGlobal.pintarChecks(checkboxDiv,functionsGlobal.filterupComingEvents(data.events,data.currentDate));
-functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(data.events, data.currentDate));
+        functionsGlobal.pintarChecks(checkboxDiv, functionsGlobal.filterupComingEvents(data.events, data.currentDate));
+        functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(data.events, data.currentDate));
 
-inputBuscar.addEventListener('input', (evento) => {
-    let changeCategory = document.querySelectorAll("input[type=checkbox]:checked");
-    let arrayChecks = functionsGlobal.filtradoChecks(changeCategory,data.events);
+        inputBuscar.addEventListener('input', (evento) => {
+            let changeCategory = document.querySelectorAll("input[type=checkbox]:checked");
+            let arrayChecks = functionsGlobal.filtradoChecks(changeCategory, data.events);
 
-    if (inputBuscar.value !== 0) {
-        arrayChecks = functionsGlobal.filtradoBuscar(inputBuscar.value, arrayChecks);
-    }
-    functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(arrayChecks,data.currentDate));
-});
+            if (inputBuscar.value !== 0) {
+                arrayChecks = functionsGlobal.filtradoBuscar(inputBuscar.value, arrayChecks);
+            }
+            functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(arrayChecks, data.currentDate));
+        });
 
-checkboxDiv.addEventListener('change', (evento) => {
-    let changeCategory = document.querySelectorAll("input[type=checkbox]:checked");
-    let arrayChecks = functionsGlobal.filtradoChecks(changeCategory,data.events);
+        checkboxDiv.addEventListener('change', (evento) => {
+            let changeCategory = document.querySelectorAll("input[type=checkbox]:checked");
+            let arrayChecks = functionsGlobal.filtradoChecks(changeCategory, data.events);
 
-    if (inputBuscar.value !== 0) {
-        arrayChecks = functionsGlobal.filtradoBuscar(inputBuscar.value, arrayChecks);
-    }
+            if (inputBuscar.value !== 0) {
+                arrayChecks = functionsGlobal.filtradoBuscar(inputBuscar.value, arrayChecks);
+            }
 
-    functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(arrayChecks,data.currentDate));
-});
+            functionsGlobal.pintarCartas(cartasDiv, functionsGlobal.filterupComingEvents(arrayChecks, data.currentDate));
+        });
+
+    })
+
 
 
 
